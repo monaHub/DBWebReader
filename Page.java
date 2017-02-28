972c075c577b1ad7c15d5d3a9fb0735a9372678c
Mona <m_arjang@yahoo.com>
2017-02-28 06:19:00 -0600

    start

---
+++ Page.java
@@ -0,0 +1,47 @@
+
+public class Page {
+	private int id;
+	String address;
+	private String fech_time;
+	private int lengths;
+	private String modified_time;
+	private String type;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getFech_time() {
+		return fech_time;
+	}
+	public void setFech_time(String fech_time) {
+		this.fech_time = fech_time;
+	}
+	public int getLengths() {
+		return lengths;
+	}
+	public void setLengths(int lengths) {
+		this.lengths = lengths;
+	}
+	public String getModified_time() {
+		return modified_time;
+	}
+	public void setModified_time(String modified_time) {
+		this.modified_time = modified_time;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+
+}
